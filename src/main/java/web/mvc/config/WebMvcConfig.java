28104c059681d1cf7156fc6dc43db577fa4e6069
Minseo Shin <minseo9451@gmail.com>
2026-06-02 10:09:47 +0900

    slack alert

--- src/main/java/web/mvc/config/WebMvcConfig.java
+++ src/main/java/web/mvc/config/WebMvcConfig.java
@@ -1,26 +1,29 @@
 package web.mvc.config;
+
 import org.springframework.context.annotation.Configuration;
-import 
-org.springframework.web.servlet.config.annotation.CorsRegistry;
-import 
-org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import 
-org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import
+        org.springframework.web.servlet.config.annotation.CorsRegistry;
+import
+        org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import
+        org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
-* WebMvcConfigurer 를 이용해서 @CrossOrigin 글로벌 설정
-* */
+ * WebMvcConfigurer 를 이용해서 @CrossOrigin 글로벌 설정
+ *
+ */
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-     registry.addMapping("/**")
-    // .allowedOrigins("http://localhost:5173","http://localhost:4173")
-     //.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
-      .allowedOrigins("http://43.203.170.229", "http://43.203.170.229:80")
-      .allowedOrigins("http://heejung.n-e.kr", "https://heejung.n-e.kr")
-     .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
-  }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // .allowedOrigins("http://localhost:5173","http://localhost:4173")
+                //.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
+                .allowedOrigins("http://43.203.170.229", "http://43.203.170.229:80")
+                .allowedOrigins("http://heejung.n-e.kr", "https://heejung.n-e.kr")
+                .allowedOrigins("http://3.36.74.121", "http://3.36.74.121:80")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
 }
